859c01907a96b85c550a728e874559fd81e3b615
mikhail <mikhail.mannai@esprit.tn>
2021-11-11 16:39:52 +0100

    crud

--- connection.cpp
+++ connection.cpp
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("test-bd");//inserer le nom de la source de données ODBC
-db.setUserName("mikhail");//inserer nom de l'utilisateur
-db.setPassword("mikhail");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("test-bd");
+db.setUserName("mikhail");
+db.setPassword("mikhail");
 
 
 
